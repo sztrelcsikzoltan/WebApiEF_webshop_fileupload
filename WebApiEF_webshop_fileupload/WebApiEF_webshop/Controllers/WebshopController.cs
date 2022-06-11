0a693d52a0ebac6a464a2051e85a5a900fb43cf8
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-11 23:40:32 +0200

    Add error handling to controller actions with status code 400 response (BadRequest)

--- WebApiEF_webshop_fileupload/WebApiEF_webshop/Controllers/WebshopController.cs
+++ WebApiEF_webshop_fileupload/WebApiEF_webshop/Controllers/WebshopController.cs
@@ -12,7 +12,7 @@ namespace WebApiEF_webshop.Controllers
     public class WebshopController : Controller
     {
         private readonly WebshopService service;
-        public WebshopController( WebshopService service)
+        public WebshopController(WebshopService service)
         {
             this.service = service;
         }
@@ -25,59 +25,101 @@ namespace WebApiEF_webshop.Controllers
             return service.GetProducts();
         }
         */
-        
-        public  IActionResult GetProducts()
+
+        public IActionResult GetProducts()
         {
             IEnumerable<DTO_Product> dTO_Products = service.GetProducts();
             return Ok(dTO_Products);
         }
-        
+
         [HttpGet]
         [Route("/GetProduct")]
         public IActionResult GetProduct(int productId)
         {
-            Product product = service.GetProduct(productId);
-            return Ok(product);
+            try
+            {
+                Product product = service.GetProduct(productId);
+                return Ok(product);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetProductsByCustomerId")]
         public IActionResult GetProductsByCustomerId([Required] int customerId)
         {
-            IEnumerable<Product> products = service.GetProductsByCustomerId(customerId);
-            return Ok(products);
+            try
+            {
+                IEnumerable<Product> products = service.GetProductsByCustomerId(customerId);
+                return Ok(products);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetOnlyProductsByCustomerId")]
         public IActionResult GetOnlyProductsByCustomerId([Required] int customerId)
         {
-            IEnumerable<DTO_Product> dTO_Products = service.GetOnlyProductsByCustomerId(customerId);
-            return Ok(dTO_Products);
+            try
+            {
+                IEnumerable<DTO_Product> dTO_Products = service.GetOnlyProductsByCustomerId(customerId);
+                return Ok(dTO_Products);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetProductsAndOrdersByCustomerId")]
         public IActionResult GetProductsAndOrdersByCustomerId([Required] int customerId)
         {
-            IEnumerable<Product> products = service.GetProductsAndOrdersByCustomerId(customerId);
-            return Ok(products);
+            try
+            {
+                IEnumerable<Product> products = service.GetProductsAndOrdersByCustomerId(customerId);
+                return Ok(products);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetProductsAndTotalAmountByCustomerId")]
         public IActionResult GetProductAndTotalAmountByProducIdAndCustomerId(int customerId)
         {
-            IEnumerable<DTO_ProductAndTotalAmount> dTO_ProductAndTotalAmounts = service.GetProductsAndTotalAmountByCustomerId(customerId);
-            return Ok(dTO_ProductAndTotalAmounts);
+            try
+            {
+                IEnumerable<DTO_ProductAndTotalAmount> dTO_ProductAndTotalAmounts = service.GetProductsAndTotalAmountByCustomerId(customerId);
+                return Ok(dTO_ProductAndTotalAmounts);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetProductAndTotalAmountByProductIdAndCustomerId")]
         public IActionResult GetProductAndTotalAmountByProducIdAndCustomerId(int productId, int customerId)
         {
-            DTO_ProductAndTotalAmount dTO_ProductAndTotalAmount = service.GetProductAndTotalAmountByProductIdAndCustomerId(productId, customerId);
-            return Ok(dTO_ProductAndTotalAmount);
+            try
+            {
+                DTO_ProductAndTotalAmount dTO_ProductAndTotalAmount = service.GetProductAndTotalAmountByProductIdAndCustomerId(productId, customerId);
+                return Ok(dTO_ProductAndTotalAmount);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
 
@@ -85,33 +127,61 @@ namespace WebApiEF_webshop.Controllers
         [Route("/GetProductsAndOrderIdsByCustomerId")]
         public IActionResult GetProductsAndOrderIdsByCustomerId([Required] int customerId)
         {
-            IEnumerable<DTO_ProductAndOrderIds> dTO_ProductAndOrderIds = service.GetProductsAndOrderIdsByCustomerId(customerId);
-            return Ok(dTO_ProductAndOrderIds);
+            try
+            {
+                IEnumerable<DTO_ProductAndOrderIds> dTO_ProductAndOrderIds = service.GetProductsAndOrderIdsByCustomerId(customerId);
+                return Ok(dTO_ProductAndOrderIds);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetProductsByCustomerIdFromDate")]
         public IActionResult GetProductsByCustomerIdFromDate(int customerId, DateTime fromDate)
         {
-            IEnumerable<Product> products = service.GetProductsByCustomerIdFromDate(customerId, fromDate);
-            return Ok(products);
+            try
+            {
+                IEnumerable<Product> products = service.GetProductsByCustomerIdFromDate(customerId, fromDate);
+                return Ok(products);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        
+
         [HttpGet]
         [Route("/GetProductsByOrderId")]
         public IActionResult GetProductsByOrderId(int orderId)
         {
-            IEnumerable<Product> products = service.GetProductsByOrderId(orderId);
-            return Ok(products);
+            try
+            {
+                IEnumerable<Product> products = service.GetProductsByOrderId(orderId);
+                return Ok(products);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetTotalProductsByCustomerIdAndProductId")]
         public IActionResult GetTotalProductsByCustomerIdAndProductId(int customerId, int productId)
         {
-            string totalProducts = service.GetTotalProductsByCustomerIdAndProductId(customerId, productId);
-            return Ok(totalProducts);
+            try
+            {
+                string totalProducts = service.GetTotalProductsByCustomerIdAndProductId(customerId, productId);
+                return Ok(totalProducts);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
 
@@ -119,72 +189,135 @@ namespace WebApiEF_webshop.Controllers
         [Route("/GetOrdersByCustomerId")]
         public IActionResult GetOrdersByCustomerId(int customerId)
         {
-            IEnumerable<Order> orders = service.GetOrdersByCustomerId(customerId);
-            return Ok(orders);
+            try
+            {
+                IEnumerable<Order> orders = service.GetOrdersByCustomerId(customerId);
+                return Ok(orders);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("/GetOrdersByProductName")]
         public IActionResult GetOrdersByProductName(string productName)
         {
-            IEnumerable<Order> orders = service.GetOrdersByProductName(productName);
-            return Ok(orders);
+            try
+            {
+                IEnumerable<Order> orders = service.GetOrdersByProductName(productName);
+                return Ok(orders);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route(("/GetOrdersByProductNameAndCustomerName"))]
         public IActionResult GetOrdersByProductNameAndCustomerName(string productName, string customerName)
         {
-            IEnumerable<Order> orders = service.OrdersByProductNameAndCustomerName(productName, customerName);
-            return Ok(orders);
+            try
+            {
+                IEnumerable<Order> orders = service.OrdersByProductNameAndCustomerName(productName, customerName);
+                return Ok(orders);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
         [Route("/AddOrder")]
         public IActionResult AddNewOrder(int customerId, int productId, int amount)
         {
-            string result = service.AddOrder(customerId, productId, amount);
-            return Ok(result);
+            try
+            {
+                string result = service.AddOrder(customerId, productId, amount);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
         [Route("/AddProduct")]
         public IActionResult AddProduct(Product product)
         {
-            string result = service.AddProduct(product);
-            return Ok(result);
+            try
+            {
+                string result = service.AddProduct(product);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
         [Route("/AddCustomer")]
         public IActionResult AddCustomer(Customer customer)
         {
-            string result = service.AddCustomer(customer);
-            return Ok(result);
+            try
+            {
+                string result = service.AddCustomer(customer);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut]
         [Route("/AddProductToOrderByProductIdAndOrderId")]
         public IActionResult AddProductToOrderByProductIdAndOrderId(int orderid, int productId, int amount)
         {
-            string result = service.AddProductToOrderByOrderIdAndProductId(orderid, productId, amount);
-            return Ok(result);
+            try
+            {
+                string result = service.AddProductToOrderByOrderIdAndProductId(orderid, productId, amount);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut]
         [Route("/CloseOrder")]
         public IActionResult CloseOrder(int orderId)
         {
-            string result = service.CloseOrder(orderId);
-            return Ok(result);
+            try
+            {
+                string result = service.CloseOrder(orderId);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut]
         [Route("/UpdateProduct")]
         public IActionResult UpdateProduct(Product product)
         {
-            string result = service.UpdateProduct(product);
-            return Ok(result);
+            try
+            {
+                string result = service.UpdateProduct(product);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
 
@@ -192,32 +325,60 @@ namespace WebApiEF_webshop.Controllers
         [Route("/DeleteCustomer")]
         public IActionResult DeleteCustomer(int customerId)
         {
-            string result = service.DeleteCustomer(customerId);
-            return Ok(result);
+            try
+            {
+                string result = service.DeleteCustomer(customerId);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-        
+
         [HttpDelete]
         [Route("/DeleteProduct")]
         public IActionResult DeleteProduct(int productId)
         {
-            string result = service.DeleteProduct(productId);
-            return Ok(result);
+            try
+            {
+                string result = service.DeleteProduct(productId);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-        
+
         [HttpDelete]
         [Route("/DeleteProductFromOrderByOrderIdAndProductId")]
         public IActionResult DeleteProductFromOrderByOrderIdAndProductId(int orderId, int productId)
         {
-            string result = service.DeleteProductFromOrderByOrderIdAndProductId(orderId, productId);
-            return Ok(result);
+            try
+            {
+                string result = service.DeleteProductFromOrderByOrderIdAndProductId(orderId, productId);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-        
+
         [HttpDelete]
         [Route("/DeleteOrder")]
         public IActionResult DeleteOrder(int orderId)
         {
-            string result = service.DeleteOrder(orderId);
-            return Ok(result);
+            try
+            {
+                string result = service.DeleteOrder(orderId);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
