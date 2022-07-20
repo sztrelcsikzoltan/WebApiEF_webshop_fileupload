6a15ff0242ab5590bc0c9a5129f1c3b262eb83a4
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-07-20 20:09:04 +0200

    Simplify rule for returning BadRequest response in AddProduct action

--- WebApiEF_webshop_fileupload/WebApiEF_webshop/Controllers/WebshopController.cs
+++ WebApiEF_webshop_fileupload/WebApiEF_webshop/Controllers/WebshopController.cs
@@ -320,7 +320,7 @@ namespace WebApiEF_webshop.Controllers
             try
             {
                 string result = service.AddProduct(dTO_ProductNoId);
-                if (result.Contains("already exists") || result.Contains("Please enter"))
+                if (result.Contains("was added"))
                 {
                     return BadRequest(result);
                 }
